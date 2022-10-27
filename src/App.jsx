36bbd9cd2a357707d0e116e8ee9b7c544bfc9dff
Sack <sack39@gmail.com>
2022-10-27 22:54:00 +0900

    うまく動かないのでログアウト処理抹殺

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Login from 'login/Login';
 import Admin from 'admin/Admin';
 import Home from 'home/Home';
 import Mission from 'mission/Misson';
-import Logout from 'logout/Logout';
 
 import { db } from 'common/Firebase'
 import { collection ,onSnapshot, query, doc, orderBy } from 'firebase/firestore'
@@ -145,7 +144,6 @@ function App() {
           <Route path='/mission' element={<Mission game={game} missions={missions} answereds={answereds} />} / >
           <Route path='/login/*' element={<Login game={game} />} />
           <Route path='/admin/*' element={<Admin game={game} missions={missions} answereds={answereds} />} />
-          <Route path='/logout' element={<Logout />} />
 
       </Routes>
       </BrowserRouter>
